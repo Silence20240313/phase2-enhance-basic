b649195ffaae70287ef69415e072603535a11fa5
Silence20240313 <liangxiaoli378@gmail.com>
2024-05-25 09:56:17 +0900

    2024.5.25

--- oop-app1/src/com/itheima/d10_extends_feature/Test.java
+++ oop-app1/src/com/itheima/d10_extends_feature/Test.java
@@ -5,5 +5,14 @@ public class Test {
         //目标：掌握继承的两个注意事项
         //1.Java是单继承的，一个类只能继承一个直接父类，Java中的类不支持多继承，但是支持多层继承
         //2.Object类是Java中所有类的祖宗
+        A a =new A();
+
+        B b =new B();
+
     }
 }
+
+class A {} //extends Object {}
+class B extends A{}
+//class C extends B , A{}//报错
+class D extends B{}
