ba77a2d640d9774a398874ff36e25a2ec9b8a341
Silence20240313 <liangxiaoli378@gmail.com>
2024-06-03 07:56:13 +0900

    2024.6.3

--- oop-app1/src/com/itheima/d12_extends_visit/Test.java
+++ oop-app1/src/com/itheima/d12_extends_visit/Test.java
@@ -5,5 +5,6 @@ public class Test {
         //目标：掌握子类中访问其他成员的特点：就近原则
         Z z = new Z();
         z.showName();
+        z.showMethod();
     }
 }
--- oop-app1/src/com/itheima/d12_extends_visit/Z.java
+++ oop-app1/src/com/itheima/d12_extends_visit/Z.java
@@ -1,5 +1,7 @@
 package com.itheima.d12_extends_visit;
 
+import java.sql.SQLOutput;
+
 public class Z extends F {
      String name = "子类名称";
 
@@ -8,5 +10,15 @@ public class Z extends F {
           System.out.println(name);//局部名称
           System.out.println(this.name);//子类成员变量
           System.out.println(super.name);//父类的成员变量
+     }
+
+     @Override
+     public void print1(){
+          System.out.println("=====子类的print1方法执行了========");
+     }
+
+     public void showMethod(){
+          print1();//子类的
+          super.print1();//父类的
      }
 }
