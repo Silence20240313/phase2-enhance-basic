0fda3f8030f91b02c341f97d2fd8295c6ac2e294
Silence20240313 <liangxiaoli378@gmail.com>
2024-06-10 07:57:30 +0900

    2024.6.10

---
+++ oop-app1/src/com/itheima/d13_extends_constructor/Test2.java
@@ -0,0 +1,54 @@
+package com.itheima.d13_extends_constructor;
+
+public class Test2 {
+    public static void main(String [] arges){
+        //目标：搞清楚子类构造器为什么要调用父类构造器，有啥应用场景
+        Teacher t = new Teacher("李四",36,"Java") ;
+
+    }
+}
+
+class Teacher extends People{
+    private String skill;
+
+    public Teacher(String name,int age,String skill){
+
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+}
+
+class People{
+    private String name;
+    private int age;
+
+    public People() {
+    }
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
