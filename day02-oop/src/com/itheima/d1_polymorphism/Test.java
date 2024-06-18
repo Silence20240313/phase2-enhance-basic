33ec45b4dc48796f04a167969c6e399fee55bf55
Silence20240313 <liangxiaoli378@gmail.com>
2024-06-18 20:07:01 +0900

    2024.6.18

--- day02-oop/src/com/itheima/d1_polymorphism/Test.java
+++ day02-oop/src/com/itheima/d1_polymorphism/Test.java
@@ -4,5 +4,10 @@ public class Test {
     public static void main(String[] args) {
         //目标：认识多态，对象多态 行为多态
         //1.对象多态
+        People p1 = new Teacher();
+        p1.run(); //识别技巧：编译看左边，运行看右边
+
+        People p2 = new Student();
+        p2.run();//识别技巧：编译看左边，运行看右边
     }
 }
