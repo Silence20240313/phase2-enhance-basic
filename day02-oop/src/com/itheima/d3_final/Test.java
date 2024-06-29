a52a75d43b3d0257782a65f8143ef892954067d7
Silence20240313 <liangxiaoli378@gmail.com>
2024-06-29 09:27:44 +0900

    2024.6.29

---
+++ day02-oop/src/com/itheima/d3_final/Test.java
@@ -0,0 +1,13 @@
+package com.itheima.d3_final;
+
+public class Test {
+    public static void main(String[] args) {
+        //目标：认识final的作用
+    }
+}
+
+//1.final修饰类，类不能被继承了
+final class A{}
+//class B extends A{}
+
+
