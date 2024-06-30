04c5565fb0afb52737a522d8b805f336ec472465
Silence20240313 <liangxiaoli378@gmail.com>
2024-07-01 07:57:44 +0900

    2024.7.1

--- day02-oop/src/com/itheima/d3_final/Test.java
+++ day02-oop/src/com/itheima/d3_final/Test.java
@@ -1,9 +1,28 @@
 package com.itheima.d3_final;
 
 public class Test {
+    //常量：public static final修饰的成员变量，建议名称全部大写，多个单词下划线连接
+    public static final String SCHOOL_NAME = "黑马";
+
+
+
     public static void main(String[] args) {
         //目标：认识final的作用
         //3.final可以修饰变量：总规则：只能赋值一次
+        //变量：1.  局部变量  2.成员变量(静态成员变量 实例成员变量)
+
+        final int a;
+        a = 12;
+        //a = 13;//第二次赋值，报错了
+
+        final double r = 3.14;
+        // r = 0.1;//第二次赋值，报错了
+
+        //schoolName = "白马";//第二次赋值，报错了
+    }
+
+    public static void buy(final double z){
+        //z = 0.1;//第二次赋值，报错了
     }
 }
 
